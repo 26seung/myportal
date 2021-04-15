fa96ab3b41e9113b8dbd4a06f5075d7de52c7949
이유승 <yousi7@naver.com>
2021-04-15 23:49:56 +0900

    study

---
+++ myportal/src/main/java/com/bitacademy/myportal/repository/GuestbookDaoImpl.java
@@ -0,0 +1,50 @@
+package com.bitacademy.myportal.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bitacademy.myportal.vo.GuestbookVo;
+
+@Repository								//	이름 명시 안하면 클래스 이름을 기반으로 자동 명명
+										//	--> guestbookDaoImpl 이름을 bean 등록
+										//	@Repository("guestbookDao")
+public class GuestbookDaoImpl implements GuestbookDao {
+
+//	데이터 소스 연결
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Override
+	public List<GuestbookVo> selectAll() {
+			//	TODO: 예외 전환 처리
+			List<GuestbookVo> list = sqlSession.selectList("guestbook.selectAll");
+			System.out.println("방명록:" + list);
+			return list;
+			
+		// 가상 데이터
+				//	TODO: 실제 DB와 연결
+//				List<GuestbookVo> list = new ArrayList<>();
+//				// Long no, String name, String password, String content, Date regDate
+//				list.add(new GuestbookVo(1L, "홍길동", "1234", "왔다 가요", new Date()));
+//				list.add(new GuestbookVo(2L, "장길산", "pass", "저두요", new Date()));
+//				list.add(new GuestbookVo(3L, "전우치", "test", "반갑습니다", new Date()));
+//				return list;
+		
+	}
+
+	@Override
+	public int insert(GuestbookVo vo) {
+		int insertedCount = sqlSession.insert("guestbook.insert", vo);
+		return insertedCount;
+	}
+
+	@Override
+	public int delete(GuestbookVo vo) {
+		int deletedCount = sqlSession.delete("guestbook.delete", vo);
+		return deletedCount;
+	}
+
+}
--- myportal/src/main/webapp/WEB-INF/views/error/exception.jsp
+++ myportal/src/main/webapp/WEB-INF/views/error/exception.jsp
@@ -6,7 +6,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>My Portal</title>
+<title>Insert title here</title>
 <link rel="stylesheet"
 	href="<c:url value="/assets/css/home.css" />" />
 </head>
@@ -17,7 +17,11 @@
 		<div id="wapper">
 			<div id="content">
 				<div id="site-introduction">
-					<p>Spring으로 만든 홈페이지입니다</p>
+					<h1>OOPS!</h1>
+					
+					<h2>Error 발생!</h2>
+					<h3>${ name }</h3>
+					<p>${ message }</p>
 				</div>
 			</div>
 		</div>
