1a79af98dd2dbe73679af2a2c46e7d27f4d24cb9
이유승 <yousi7@naver.com>
2021-04-19 15:08:05 +0900

    spring학습

---
+++ myportal/src/main/java/com/bitacademy/myportal/repository/BoardDao.java
@@ -0,0 +1,13 @@
+package com.bitacademy.myportal.repository;
+
+import java.util.List;
+
+import com.bitacademy.myportal.vo.BoardVo;
+
+public interface BoardDao {
+
+	public List<BoardVo> selectAll();		// 	계시물 목록 조회용
+	public int insert(BoardVo co);			//	게시물 추가용
+	public BoardVo getContent(Long no);		//	게시물 조회용
+	public int update(BoardVo vo);			//	게시물 수정용
+}
